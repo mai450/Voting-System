fefdebf93cb273125688516623bd23ef3d1b4f95
Mai Awad <maiawad450@gmail.com>
2024-01-30 18:18:37 +0200

    1

---
+++ lib/Shared/Colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color mainColor = const Color(0xFF008753);
+  static Color secondaryColor = const Color(0xFFC8EADD);
+  static Color backgroundColor = const Color(0xFFF3FAF7);
+  static Color mainTextColor = const Color(0xFF000000);
+  static Color secondaryTextColor = const Color(0xFFA4A4A4);
+  static Color borderColor = const Color(0xFFC7C7C7);
+  static Color iconsColor = const Color(0xFF777272);
+  static Color grayColor = const Color(0xFFF3EFEF);
+  static Color errorColor = const Color(0xFFE91A1A);
+}
---
+++ lib/Shared/Fonts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static TextStyle fontfamily = GoogleFonts.inter();
+  static double fontsize24 = 24;
+  static double fontsize16 = 16;
+  static double fontsize14 = 14;
+  static double fontsize12 = 12;
+  static FontWeight fontWeight400 = FontWeight.w400;
+  static FontWeight fontWeight600 = FontWeight.w600;
+  static FontWeight fontWeight700 = FontWeight.w700;
+}
