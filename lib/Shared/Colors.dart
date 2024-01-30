c093620f51d000deebe9b36097a2eb0bb9f1c8d8
Mai Awad <maiawad450@gmail.com>
2024-01-30 22:35:55 +0200

    3

--- lib/Shared/Colors.dart
+++ lib/Shared/Colors.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color mainColor = const Color(0xFF008753);
-  static Color secondaryColor = const Color(0xFFC8EADD);
+  static Color eventButtonColor = const Color(0xFFD7F8EB);
   static Color backgroundColor = const Color(0xFFF3FAF7);
   static Color mainTextColor = const Color(0xFF000000);
-  static Color secondaryTextColor = const Color(0xFFA4A4A4);
+  static Color secondaryTextColor = const Color(0xFF777272);
   static Color borderColor = const Color(0xFFC7C7C7);
-  static Color iconsColor = const Color(0xFF777272);
-  static Color grayColor = const Color(0xFFF3EFEF);
-  static Color errorColor = const Color(0xFFE91A1A);
+  static Color navigationBarColor = const Color(0xFF4F4F4F);
 }
